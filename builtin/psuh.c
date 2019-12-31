703217ff195b26317c90a4834c79dbbd8feaa929
Jeff Haemer <jeffrey.haemer@gmail.com>
2019-12-31 23:04:43 +0000

    Document git-psuh.
    Make a man page.
    Tweak psuh.c to permit `git psuh -h` to handle the `-h` argument correctly.
    Adjust command-list.txt to let `git help git` (and git help psuh) know about the command and its documentation.
    
    Signed-off-by: Jeff Haemer <jeffrey.haemer@gmail.com>

--- builtin/psuh.c
+++ builtin/psuh.c
@@ -1,15 +1,28 @@
 #include "builtin.h"
+#include "wt-status.h"
 #include "config.h"
+#include "parse-options.h"
+
+static const char * const psuh_usage[] = {
+        N_("git psuh [<arg>...]"),
+        NULL,
+};
 
 int cmd_psuh(int argc, const char **argv, const char *prefix)
 {
 	int i;
 	const char *cfg_name;
+	struct wt_status status;
         struct commit *c = NULL;
         struct strbuf commitline = STRBUF_INIT;
 
+        struct option options[] = {
+                OPT_END()
+        };
+
         printf(_("Pony saying 'Um, Hello'\n"));
 
+	argc = parse_options(argc, argv, prefix, options, psuh_usage, 0);
         printf(Q_("Your args (there is %d):\n",
                   "Your args (there are %d):\n", argc), argc);
         for (i = 0; i < argc; i++)
@@ -23,6 +36,10 @@ int cmd_psuh(int argc, const char **argv, const char *prefix)
                 printf(_("No name is found in config\n"));
         else
                 printf(_("Your name: %s\n"), cfg_name);
+
+	wt_status_prepare(the_repository, &status);
+        git_config(git_default_config, &status);
+        printf(_("Your current branch: %s\n"), status.branch);
 
         c = lookup_commit_reference_by_name("origin/master");
 
